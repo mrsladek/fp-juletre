972ed22d4b04bba898a8e811de33ce2e417fc9a9
Michal J. Sladek <michal.sladek@nav.no>
2021-12-03 11:23:30 +0100

    Init juletre

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM navikt/java:17
+COPY target/*.jar app.jar
---
+++ src/main/java/no/nav/foreldrepenger/jul/Kalender.java
@@ -0,0 +1,46 @@
+package no.nav.foreldrepenger.jul;
+
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.Set;
+
+import org.springframework.http.CacheControl;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Kalender {
+
+    private static final Set<String> team = Set.of("AP", "AD", "AAL", "AGA", "BBS", "CH", "EW", "GR", "JEJ", "JOL", "KQ", "KJ", "MLW", "MJS", "PE", "SR", "SHP", "TR");
+
+    @GetMapping("/winner")
+    ResponseEntity<String> getWinner() {
+        String[] teamArray = team.toArray(new String[0]);
+        Random random = new Random();
+        var winner = teamArray[random.nextInt(team.size())];
+        return ResponseEntity.ok().contentType(MediaType.TEXT_PLAIN).cacheControl(CacheControl.noCache())
+                .body(printTree() + LocalDate.now().toString() + System.getProperty("line.separator") + winner + " du vant! Gratulerer!");
+    }
+
+    private static StringBuffer printTree() {
+        StringBuffer buffer = new StringBuffer();
+        printTriangle(buffer, 10, 0, 10);
+        printTriangle(buffer, 5, 5, 15);
+        printTriangle(buffer, 0, 10, 20);
+        printSquare(buffer, 15, 9, 4);
+        return buffer;
+    }
+
+    private static void printTriangle(StringBuffer buffer, int indent, int start, int size) {
+        for (int row = start; row < size; row++)
+            buffer.append(" ".repeat(size - row - 1 + indent)).append("*".repeat(1 + 2 * row)).append(System.getProperty("line.separator"));
+    }
+
+    private static void printSquare(StringBuffer buffer, int indent, int width, int height) {
+        for (int row = 0; row < height; row++)
+            buffer.append(" ".repeat(indent)).append("*".repeat(width)).append(System.getProperty("line.separator"));
+    }
+
+}
